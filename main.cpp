86546915352fe2059bfa1b552a6b9e4b7920e2f6
Berkay SARICA <berkaysarica2005@gmail.com>
2025-03-12 00:16:22 +0300

    Implemented mouse drag-drop action

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <SFML/Window/Event.hpp>
 #include "configuration.h"
 #include <ctime>
 using namespace sf;
@@ -10,11 +11,21 @@ struct gem {
     gem() {match = 0;}
 }grid[9][9];
 
+void swap(gem g1, gem g2) {
+    std::swap(g1.column,g2.column);
+    std::swap(g1.row, g2.row);
+    std::swap(g1.x, g2.x);
+    std::swap(g1.y, g2.y);
+
+    grid[g1.row][g1.column] = g1;
+    grid[g2.row][g2.column] = g2;
+}
+
 int main()
 {
     std::srand(std::time(nullptr));
 
-    RenderWindow window(VideoMode({630, 630}), "3-match Game");
+    RenderWindow window(VideoMode({630, 630}), "3-match Game", sf::Style::Titlebar | sf::Style::Close);
     window.setFramerateLimit(60);
 
     const Texture t1("assets/board.png");
@@ -33,10 +44,31 @@ int main()
         }
     }
 
+    int in_i = -1;
+    int in_j = -1;
+
     while (window.isOpen()) {
-        while (const std::optional event = window.pollEvent()) {
+        while (const std::optional<Event> event = window.pollEvent()) {
             if (event->is<Event::Closed>())
                 window.close();
+
+            if (auto mouseEvent = event->getIf<Event::MouseButtonPressed>()) {
+                in_i = mouseEvent->position.y / (CELL_SIZE * SCALE);
+                in_j = mouseEvent->position.x / (CELL_SIZE * SCALE);
+            }
+            if (auto mouseEvent = event->getIf<Event::MouseButtonReleased>()) {
+                int final_i = mouseEvent->position.y / (CELL_SIZE * SCALE);
+                int final_j = mouseEvent->position.x / (CELL_SIZE * SCALE);
+                if (in_i == final_i && in_j != final_j) {
+                    if (-1 <= final_j - in_j && final_j - in_j <= 1) {
+                        swap(grid[in_i][in_j], grid[final_i][final_j]);
+                    }
+                } else if (in_j == final_j && in_i != final_i) {
+                    if (-1 <= final_i - in_i && final_i - in_i <= 1) {
+                        swap(grid[in_i][in_j], grid[final_i][final_j]);
+                    }
+                }
+            }
         }
         //draw
         window.clear();
