18b78312996829308f37602cbf0dad4a130b5648
Berkay SARICA <berkaysarica2005@gmail.com>
2025-03-11 15:14:35 +0300

    Assets files updated, random gem generation implemented.

---
+++ configuration.h
@@ -0,0 +1,11 @@
+#ifndef CONFIGURATION_H
+#define CONFIGURATION_H
+
+const int CELL_SIZE = 14;
+const int BOARD_SIZE = 9;
+const float SCALE = 5.0f;
+const int GEM_SIZE = 12;
+const int SCALED_GEM_SIZE = GEM_SIZE * SCALE;
+const float offset = (SCALED_GEM_SIZE - CELL_SIZE * SCALE) / 2.0f;
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,26 +1,57 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include "configuration.h"
+#include <ctime>
 using namespace sf;
+
+struct gem {
+    int row{},column{},type{},match;
+    float x{},y{};
+    gem() {match = 0;}
+}grid[9][9];
 
 int main()
 {
-    RenderWindow window(VideoMode({516, 516}), "3-match Game");
+    std::srand(std::time(nullptr));
 
-    const Texture texture("assets/board_9x9_dark_grey.png");
-    std::cout << texture.getSize().x;
-    Sprite sprite(texture);
-    sprite.setScale({3,3});
+    RenderWindow window(VideoMode({630, 630}), "3-match Game");
+    window.setFramerateLimit(60);
 
+    const Texture t1("assets/board.png");
+    const Texture t2("assets/gems.png");
+
+    Sprite background(t1), gems(t2);
+    background.setScale({5,5});
+
+    for (int i = 0; i < BOARD_SIZE; ++i) {
+        for (int j = 0; j < BOARD_SIZE; ++j) {
+            grid[i][j].type = std::rand() % 5;
+            grid[i][j].column = j;
+            grid[i][j].row = i;
+            grid[i][j].x = j * CELL_SIZE * SCALE - offset;
+            grid[i][j].y = i * CELL_SIZE * SCALE - offset;
+        }
+    }
 
     while (window.isOpen()) {
         while (const std::optional event = window.pollEvent()) {
             if (event->is<Event::Closed>())
                 window.close();
         }
-
+        //draw
         window.clear();
 
-        window.draw(sprite);
+        window.draw(background);
+
+        for (int i = 0; i < BOARD_SIZE; ++i) {
+            for (int j = 0; j < BOARD_SIZE; ++j) {
+                gem g = grid[i][j];
+                gems.setTextureRect(IntRect({g.type*13, 0}, {12,12}));
+                gems.setScale({SCALE,SCALE});
+                gems.setPosition({g.x,g.y});
+                window.draw(gems);
+            }
+        }
 
         window.display();
     }
